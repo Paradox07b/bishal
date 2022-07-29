c5bbec1702a3bdedb8bbf911cec05fa7fec32c4b
Paradox07b <110159279+Paradox07b@users.noreply.github.com>
2022-07-29 17:47:30 +0545

    Add files via upload

---
+++ my website/Javascript/index.js
@@ -0,0 +1,8 @@
+// for toggle menu
+var navLinks = document.getElementById("navLinks");
+function hidemenu(){
+    navLinks.style.left = "-100%";
+}
+function showmenu(){
+    navLinks.style.left = "0";
+}
